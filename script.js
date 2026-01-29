7e10d075346a3434e1158e6b9e01a07e438192b5
arwaa3493-lang <arwaa3493@gmail.com>
2026-01-29 15:50:57 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const hbInput = documrnt.getElementById("hb");
+const rbcInput = document.getElementById("rbc");
+const wbcInput = document.getElementById("wbc");
+const diagnoseButton = document.getElementById("diagnoseBtn");
+const diagnosisBox = document.getElementById('diagnosis");
+diagnoseButton.addEventListener("click", function() }
+                                const hb = parseFloat(hbInput.value);
+const rbc = parseFloat(rbcInput.value);
+const wbc = parseFloat(wbcInput.value);
+console.log(hb, rbc, wbc); 
+{(;

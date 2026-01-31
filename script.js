5b84669812392da3b1cc7b6168dedd0a5de7d992
arwaa3493-lang <arwaa3493@gmail.com>
2026-01-31 13:51:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+const referenceRange 
 const hbInput = documrnt.getElementById("hb");
 const rbcInput = document.getElementById("rbc");
 const wbcInput = document.getElementById("wbc");
 const diagnoseButton = document.getElementById("diagnoseBtn");
 const diagnosisBox = document.getElementById("diagnosis");
-diagnoseButton.addEventListener("click", function() {
-                                const hb = parseFloat(hbInput.value);
-const rbc = parseFloat(rbcInput.value);
-const wbc = parseFloat(wbcInput.value);
-console.log(hb, rbc, wbc); 
-});
+const ranges = {
+  ,hb: {low: 12, high: 16 }
+  ,rbc: { low: 4.0, high: 5.5 }
+  , wbc: { low: 4.0, high: 11.0 }
+;}
+diagnoseBtn.addEventListener("click", function() {
+  conset hb = parseFloat(hbInput.value);
+  conset rbc = parseFloat(rbcInput.value);
+  conset wbc = parseFloar9wbcInput.value);
+let result = "";
+  if (hb < ranges.hb.low && rbc < ranges.rbc.low){
+    result = "Pattern suggests Anemia mainly due to blood loss or Iron deficiency.";
+} 
+  else if (hb > ranges.hb.high && rbc > ranges.rbc.high) {
+    result = "likely the patient lives on high altitude. However, pathologically it migh be Polycythemia. check blood film.";
+  }
+  else if (wbc > ranges.wbc.high) {
+    result = " pattern suggest possible infection or certain inflammation commonly. However, it might be leukemia or a type of Autoimmune disease. Check blood film.";
+  }
+  else { 
+    result = " Values are within normal limits or inconclusive." 
+      }
+  DiagnosisBox.value = result; 
+  ;)}
+  
+  
+    
